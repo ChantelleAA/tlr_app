3ef4d944bc1d57a4b56a9801b2c088a8574a25d2
ChantelleAA <chantelle.amoako-atta@aims.ac.rw>
2025-06-02 12:14:15 +0000

    add new models structure

--- tlr_helper/start.sh
+++ tlr_helper/start.sh
@@ -4,7 +4,7 @@ echo "Applying database migrations..."
 python manage.py migrate --noinput
 
 echo "Loading initial fixture data..."
-python manage.py loaddata suggestor/fixtures/initial_data.json || echo "Fixture load skipped (error or already loaded)"
+python manage.py load_fixtures_safe  || echo "Fixture load skipped (error or already loaded)"
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
